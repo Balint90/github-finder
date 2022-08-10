afa0de37d4eeb5b46ed3102bd0a140c951607282
Bálint Csertán <Balint90@users.noreply.github.com>
2022-08-10 15:21:39 +0200

    Move Files to right folder Add Loading, UserResults and UserItem

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from "../users/UserResults"
+
+function Home() {
+    return (
+        <div>
+            {/* SEARCH COMPONENT */}
+            <UserResults />
+
+        </div>
+    )
+}
+
+export default Home
